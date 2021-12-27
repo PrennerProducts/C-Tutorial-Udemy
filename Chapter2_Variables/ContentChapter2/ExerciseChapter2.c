19751d827f83a855fbe74a7936897aea046be6ee
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2021-12-27 18:10:29 +0100

    Refactoring

--- Chapter2_Variables/ContentChapter2/ExerciseChapter2.c
+++ Chapter2_Variables/ContentChapter2/ExerciseChapter2.c
@@ -4,6 +4,11 @@ int main(){
     int age;
     printf("Please enter your age in yeras: ");
     scanf("%d", &age);
+    if (age < 1){
+        printf("ERROR 1:Ungültige Eingabe! Bitte gib eine positive Ganzzahl ein");
+        return 1;
+    }
+
     int days = age *365;
     int hours = days * 24;
     int minutes = hours * 60;
