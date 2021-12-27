5eb67f96aeb7e85360a4975427644e064b055b65
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2021-12-27 17:58:43 +0100

    Aufgabe Kabittel 2

--- Chapter2_Variables/ContentChapter2/ExerciseChapter2.c
+++ Chapter2_Variables/ContentChapter2/ExerciseChapter2.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
+    int age;
+    printf("Please enter your age in yeras: ");
+    scanf("%d", &age);
+    int days = age *365;
+    int hours = days * 24;
+    int minutes = hours * 60;
+    int secounds = minutes * 60;
+    printf("You are living since %d days\n", days);
+    printf("You are living since %d hours\n", hours);
+    printf("You are living since %d minutes\n", minutes);
+    printf("You are living since %d seconds\n", secounds);
+
 
 
     return 0;
---
+++ Chapter2_Variables/first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int my_number = 42000;
+
+    printf("%d", my_number);
+    return 0;
+}
