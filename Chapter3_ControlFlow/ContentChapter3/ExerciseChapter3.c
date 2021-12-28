25738aa09a7fa229886dbe17fba7b0a9b5950d87
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2021-12-28 19:42:33 +0100

    Exercise 3

--- Chapter3_ControlFlow/ContentChapter3/ExerciseChapter3.c
+++ Chapter3_ControlFlow/ContentChapter3/ExerciseChapter3.c
@@ -2,6 +2,20 @@
 
 int main()
 {
+    int number;
+    int reminder;
+    printf("Please enter a valid unsigned integer!");
+    scanf("%d", &number);
+
+    reminder = number % 3;
+
+    if(number % 3 == 0){
+        printf("The number you entered is div. by 3!\n");
+    }
+    else{
+        printf("The number you entered is not div. by 3!\n");
+    }
+    printf("The reminder of the operatio nis: %d\n",reminder);
 
     return 0;
 }
